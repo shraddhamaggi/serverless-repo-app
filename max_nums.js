e7cfdf449e471ca6326ea2bca7e872b2db2212dd
Shraddha Gupta <sgupta@Shraddhas-MacBook-Pro.local>
2022-07-11 13:52:33 -0700

    commiting code to the max branch

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,9 @@
 // start coding your function here!
+
+function find_max(arr) {
+    let max = Math.max.apply(null, arr)
+    return max
+}
+let arr = [22, 13, 4]
+console.log("Maximum value: " + find_max(arr))
+exports.find_max = find_max
