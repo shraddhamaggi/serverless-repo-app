9bb14701b1309a9ffa2cef26175df70ffb02553e
Shraddha Gupta <sgupta@Shraddhas-MacBook-Pro.local>
2022-07-11 13:56:57 -0700

    re-committing code to the max branch after removing example array

--- max_nums.js
+++ max_nums.js
@@ -4,6 +4,6 @@ function find_max(arr) {
     let max = Math.max.apply(null, arr)
     return max
 }
-let arr = [22, 13, 4]
-console.log("Maximum value: " + find_max(arr))
+//let arr = [22, 13, 4]
+//console.log("Maximum value: " + find_max(arr))
 exports.find_max = find_max
